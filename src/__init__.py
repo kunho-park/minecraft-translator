dde8fa902abc4f6e0f35123e5f17b3ff8545bc98
kunho-park <kunho-park@users.noreply.github.com>
2026-01-18 13:59:11 +0900

    Release v2.1.2

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,7 @@ from .models import Glossary, TranslationTask
 from .pipeline import PipelineConfig, PipelineResult, TranslationPipeline, run_pipeline
 from .scanner import ModpackScanner, ScanResult, scan_modpack
 
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 
 __all__ = [
     # Pipeline
